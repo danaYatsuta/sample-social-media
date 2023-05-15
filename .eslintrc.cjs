4f10827d4bfd234f24d1c72af91c29dea887d370
Dana Yatsuta <danayatsuta@gmail.com>
2023-05-15 16:16:13 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'airbnb-base'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'quote-props': ['error', 'consistent'],
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+};
